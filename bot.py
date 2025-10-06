751631b42d108541001d0698ead15d69d99e6abf
Umar FarooQ <78466453+Umar-444@users.noreply.github.com>
2025-10-06 13:41:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ import time
 from datetime import datetime, timedelta
 
 # Load credentials from environment variables
-api_key = os.getenv("API_KEY")
-api_secret = os.getenv("API_KEY_SECRET")
-access_token = os.getenv("ACCESS_TOKEN")
-access_secret = os.getenv("ACCESS_TOKEN_SECRET")
+api_key = os.getenv("pgu7dzgmZI2PMLnuLdLawTat4")
+api_secret = os.getenv("pq2kz6Hwq9qTpNPNIxrWwermOX2uBqaoUxbB2l0jXHPOQnTpiu")
+access_token = os.getenv("1406339665578319875-4XU0ERx127CLcOxNQbOlsNfvPYJHJR")
+access_secret = os.getenv("dptk7L6yPGN9N7iz4r8NBlw38h0hy0oS57lyb0bP6eU0d")
 
 # Authenticate
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_secret)
